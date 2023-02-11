c4c0a36e7501efff3cd633272e31d93fc23494ef
Howard Huang <howardhuang2004@gmail.com>
2023-02-11 15:26:14 -0800

    add api requests to render movie posters

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
 
-function Row({ title }) {
+const base_url = "https://image.tmbd.org/t/p/original";
+
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData(){
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.table(movies);
+
   return (
-    <div>
+    <div className="row">
         <h2>{title}</h2>
-
-        {/* container -> posters */}
+        <div className="row__posters">
+          {movies.map(movie => (
+            <img 
+              className="row__poster"
+              src={`${base_url}${movie.poster_path}`} 
+              alt={movie.name}
+            />
+          ))}
+        </div>
     </div>
   )
 }
