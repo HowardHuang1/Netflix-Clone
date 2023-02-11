4f4852d9a4b0a280db4cf7fdfa93c1db05efe015
Howard Huang <howardhuang2004@gmail.com>
2023-02-11 15:30:42 -0800

    fix errors with movie images rendering

--- src/Row.js
+++ src/Row.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 
-const base_url = "https://image.tmbd.org/t/p/original";
+const base_url = "https://image.tmdb.org/t/p/original"
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    async function fetchData(){
+
+    async function fetchData() {
+
       const request = await axios.get(fetchUrl);
       setMovies(request.data.results);
       return request;
@@ -15,22 +17,18 @@ function Row({ title, fetchUrl }) {
     fetchData();
   }, [fetchUrl]);
 
-  console.table(movies);
-
   return (
     <div className="row">
-        <h2>{title}</h2>
-        <div className="row__posters">
-          {movies.map(movie => (
-            <img 
-              className="row__poster"
-              src={`${base_url}${movie.poster_path}`} 
-              alt={movie.name}
-            />
-          ))}
-        </div>
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {movies.map(movie => {
+          return <img
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name} />
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
