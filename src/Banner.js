4ac425e92e70416169aa9dd74dd0026e2763ada6
Howard Huang <howardhuang2004@gmail.com>
2023-02-12 15:58:55 -0800

    finish styling banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import requests from './requests';
+import './Banner.css';
 
 function Banner() {
     const [movie, setMovie] = useState([]);
@@ -23,6 +24,10 @@ function Banner() {
 
     console.log(movie);
 
+    function truncate(str, n){
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
+
     return (
         <header className="banner"
             style={{
@@ -34,16 +39,17 @@ function Banner() {
             }}
         >
             <div className="banner__contents">
-                <h1>
-                    {movie?.title || movie?.name || movie?.original_name}
-                </h1>
+                <h1 className="banner__title"> {movie?.title || movie?.name || movie?.original_name} </h1>
                 <div className="banner__buttons">
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
 
-                <h1 className="banner__description">{movie?.overview}</h1>
+                <h1 className="banner__description">{movie?.overview}
+                {truncate(movie?.overview, 150)}
+                </h1>
             </div>
+            <div className="banner--fadeBottom" />
         </header>
     )
 }
